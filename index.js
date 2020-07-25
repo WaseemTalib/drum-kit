07f1d76edd8990b6c288d941463389c141002275
Waseem Talib <waseemltalib@gmail.com>
2020-07-25 16:39:30 +0500

    drum final

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-
 var drumLength = document.querySelectorAll(".drum").length;
 for (var i = 0; i < drumLength; i++) {
 
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         var html = this.innerHTML;
         makeSound(html);
         clickAnimation(html);
     })
 
 }
-document.addEventListener("keypress", function (keyPressed) {
+document.addEventListener("keypress", function(keyPressed) {
     var key = keyPressed.key;
     makeSound(key);
     clickAnimation(key);
@@ -21,37 +20,31 @@ clickAnimation = (html) => {
     clickedButton.classList.add("pressed");
     setTimeout(() => {
         clickedButton.classList.remove("pressed");
-   }, 100)
-   
-   
+    }, 100)
+
+
 }
 
 makeSound = (html) => {
     if (html === "w") {
         var audio = new Audio('sounds/tom-1.mp3');
         audio.play();
-    }
-    else if (html === "a") {
+    } else if (html === "a") {
         var audio = new Audio('sounds/tom-2.mp3');
         audio.play();
-    }
-    else if (html === "s") {
+    } else if (html === "s") {
         var audio = new Audio('sounds/tom-3.mp3');
         audio.play();
-    }
-    else if (html === "d") {
+    } else if (html === "d") {
         var audio = new Audio('sounds/tom-4.mp3');
         audio.play();
-    }
-    else if (html === "j") {
+    } else if (html === "j") {
         var audio = new Audio('sounds/snare.mp3');
         audio.play();
-    }
-    else if (html === "k") {
+    } else if (html === "k") {
         var audio = new Audio('sounds/crash.mp3');
         audio.play();
-    }
-    else {
+    } else {
         var audio = new Audio('sounds/kick-bass.mp3');
         audio.play();
     }
